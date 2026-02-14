2115b38b9a2ef961f6e9883992dc0cd61a2ff474
dhsellars <dhsellars@gmail.com>
2026-02-14 13:52:02 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+JELLYFIN_URL = os.getenv("JELLYFIN_URL")
+JELLYFIN_API_KEY = os.getenv("JELLYFIN_API_KEY")
+JELLYFIN_USER_ID = os.getenv("JELLYFIN_USER_ID")
+
+NTFY_TOPIC = os.getenv("NTFY_TOPIC")
+NTFY_URL = f"https://ntfy.sh/{NTFY_TOPIC}"
+
+STATE_FILE = "state.json"
+
+# How many items to fetch
+LIMIT = int(os.getenv("LIMIT", "20"))
